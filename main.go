3a13789ea0ea319716c4a1e6ff34f397902405e7
Luke Griffith <luke@griffith.cloud>
2019-07-26 22:26:55 +0100

    Added pre-commit hook

--- main.go
+++ main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-  "context"
-  "github.com/lukemgriffith/captainhook/app"
-  "log"
-  "net/http"
-  "os"
-  "os/signal"
-  "syscall"
+	"context"
+	"github.com/lukemgriffith/captainhook/app"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 
-  app := app.New()
+	app := app.New()
 
-  server := &http.Server{
-    Addr:    ":8081",
-    Handler: app,
-  }
+	server := &http.Server{
+		Addr:    ":8081",
+		Handler: app,
+	}
 
-  go server.ListenAndServe()
+	go server.ListenAndServe()
 
-  c := make(chan os.Signal, 1)
-  signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-  for {
-    s := <-c
-    log.Print("os signal recieved. processing.")
+	for {
+		s := <-c
+		log.Print("os signal recieved. processing.")
 
-    switch s {
-    case syscall.SIGTERM:
-      log.Print("SIGTERM: shutting server down gracefully")
-      server.Shutdown(context.Background())
-      return
-    }
-  }
+		switch s {
+		case syscall.SIGTERM:
+			log.Print("SIGTERM: shutting server down gracefully")
+			server.Shutdown(context.Background())
+			return
+		}
+	}
 }
