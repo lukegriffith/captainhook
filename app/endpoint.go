8c48694727bc1f645fa415dcb998aa60b60d7026
Luke Griffith <luke@griffith.cloud>
2019-07-26 23:16:51 +0100

    gofmt

--- app/endpoint.go
+++ app/endpoint.go
@@ -1,9 +1,9 @@
 package app
 
 type endpoint struct {
-  id string `json:"id"`
-  name string `json:"name"`
-  secret string `json:"secret"`
-  rules  []rule `json:"rules "`
-  source string `json:"source"`
+	id     string `json:"id"`
+	name   string `json:"name"`
+	secret string `json:"secret"`
+	rules  []rule `json:"rules "`
+	source string `json:"source"`
 }
--- app/rule.go
+++ app/rule.go
@@ -1,8 +1,8 @@
 package app
 
 type rule struct {
-  id string `json:"id"`
-  destination_url  string `json:"destination "`
-  template string `json:"template"`
-  verify_ssl string `json:"verify_ssl"`
+	id              string `json:"id"`
+	destination_url string `json:"destination "`
+	template        string `json:"template"`
+	verify_ssl      string `json:"verify_ssl"`
 }
