a1bd05b5fb299ffdd3b60bfbf70a689046b00fdd
Luke Griffith <luke@griffith.cloud>
2019-08-24 20:03:26 +0100

    Added config reloading, and changes helper / makefile.
    Added configuration reload, but currently is not working. Load a config
    item from a path but loads endpoints as nil.
    
    Changed helper script to detect and kill ```go run``` command in makefile.
    refactored functions
    
    Updated makefile to use go run over a built artifact.

--- cmd/standalone/main.go
+++ cmd/standalone/main.go
@@ -13,8 +13,6 @@ import (
 
 var data string = "/tmp/test.yaml"
 
-
-
 func main() {
 
 	config, svc := configparser.NewConfig(data)
@@ -30,26 +28,25 @@ func main() {
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGUSR1,
-    	syscall.SIGUSR2)
+		syscall.SIGUSR2)
 
 	for {
 		s := <-c
 		log.Print("os signal recieved. processing.")
 
 		switch s {
-		  case syscall.SIGTERM:
+		case syscall.SIGTERM:
 			log.Print("SIGTERM: shutting server down gracefully.")
 			server.Shutdown(context.Background())
 			return
 
-		  case syscall.SIGUSR1:
+		case syscall.SIGUSR1:
 			log.Print("SIGUSR1: reloading configuration.")
 			config.Reload()
 
-
-		  case syscall.SIGUSR2:
+		case syscall.SIGUSR2:
 			log.Print("SIGUSR2: Dump config.")
 			config.Dump()
 		}
-  }
+	}
 }
--- configparser/config.go
+++ configparser/config.go
@@ -1,116 +1,109 @@
 package configparser
 
 import (
+	"bufio"
 	"errors"
 	"github.com/lukemgriffith/captainhook"
 	"gopkg.in/yaml.v2"
-  "os"
-  "io/ioutil"
-  "bufio"
-  "log"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 type Config struct {
 	endpoints *[]captainhook.Endpoint `json:"endpoints"`
-	path string
+	path      string
 }
 
 func (c *Config) GetEndpoints() *[]captainhook.Endpoint {
 	return c.endpoints
 }
 
-
-
 func (c *Config) Reload() {
-  log.Println("loading", c.path)
-  c.reload(c.path)
+	log.Println("loading", c.path)
+	c.reload(c.path)
 }
 
 func (c *Config) Dump() {
-  log.Println(c.GetEndpoints())
+	log.Println(c.GetEndpoints())
 }
 
 func (c *Config) setEndpoint(e *[]captainhook.Endpoint) {
 
-  log.Println("new confg", e)
-  c.endpoints = e
+	log.Println("new confg", e)
+	c.endpoints = e
 }
 
 // TODO: Reload is broken and load a nil structure for endpoints. Needs to be resolved.
 
 func (c *Config) reload(path string) {
 
-  finfo, err := os.Stat(path)
-  if err != nil {
-    log.Fatal("Unable to determine file information", path)
-  }
+	finfo, err := os.Stat(path)
+	if err != nil {
+		log.Fatal("Unable to determine file information", path)
+	}
 
-  switch mode := finfo.Mode(); {
-    case mode.IsDir():
-      files, err := ioutil.ReadDir(path)
-      if err != nil {
-        log.Fatal("Unable to read directory", path)
-      }
+	switch mode := finfo.Mode(); {
+	case mode.IsDir():
+		files, err := ioutil.ReadDir(path)
+		if err != nil {
+			log.Fatal("Unable to read directory", path)
+		}
 
-      for _, f := range files {
-        c.reload(f.Name())
-      }
-    case mode.IsRegular():
-      log.Println("2")
-      file, err := os.Open(path)
+		for _, f := range files {
+			c.reload(f.Name())
+		}
+	case mode.IsRegular():
+		log.Println("2")
+		file, err := os.Open(path)
 
-      if err != nil {
-        log.Fatal("Unable to open file", path)
-      }
+		if err != nil {
+			log.Fatal("Unable to open file", path)
+		}
 
-      reader := bufio.NewReader(file)
+		reader := bufio.NewReader(file)
 
-      data, err := ioutil.ReadAll(reader)
+		data, err := ioutil.ReadAll(reader)
 
-      if err != nil {
-        log.Fatal("Unable to read file", path)
-      }
+		if err != nil {
+			log.Fatal("Unable to read file", path)
+		}
 
-      log.Println("data", data)
+		log.Println("data", data)
 
-      loadedConfig, err := LoadConfig(data)
+		loadedConfig, err := LoadConfig(data)
 
-      if err != nil {
-        log.Fatal(err, path)
-      }
+		if err != nil {
+			log.Fatal(err, path)
+		}
 
-
-      c.setEndpoint( (loadedConfig.GetEndpoints()) )
-  }
+		c.setEndpoint((loadedConfig.GetEndpoints()))
+	}
 }
 
-
 func LoadConfig(data []byte) (*Config, error) {
-
-
 
 	c := Config{nil, ""}
 	err := yaml.Unmarshal(data, &c)
 
-
-	log.Println(string (data))
+	log.Println(string(data))
 
 	if err != nil {
 		return nil, errors.New("Unable to load config from data.")
 	}
 
-  log.Println(c)
+	log.Println(c)
 
 	return &c, nil
 }
 
 func NewConfig(path string) (*Config, *EndpointService) {
 
-  e := make([]captainhook.Endpoint, 1)
+	e := make([]captainhook.Endpoint, 1)
 
-  c := &Config{&e, path}
+	c := &Config{&e, path}
 
-  c.Reload()
+	c.Reload()
 
 	return c, &EndpointService{c}
 }
