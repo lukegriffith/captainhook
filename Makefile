b276731810f071de7ba67b16fcea8f3f741656da
Luke Griffith <luke@griffith.cloud>
2019-08-26 22:02:09 +0100

    Started work on templating prototype.

--- Makefile
+++ Makefile
@@ -5,4 +5,8 @@ default: standalone
 standalone:
 	go run cmd/standalone/main.go
 
+.PHONY: templating
+templating:
+	go run cmd/templating/main.go
+
 
--- cmd/templating/main.go
+++ cmd/templating/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+var tmpl_string string = `
+{
+  "name": "{{.Name}}",
+  "type": "{{.Type}}",
+  "ID": "{{.ID}}"
+}
+`
+
+type templateStruct struct {
+	Name string
+	Type string
+	ID   int
+}
+
 func main() {
-	$END$
+
+	t := templateStruct{"Luke", "Human", 231203}
+
+	tmpl, err := template.New("rule").Parse(tmpl_string)
+
+	if err != nil {
+		log.Fatal("unable to create template")
+	}
+
+	err = tmpl.Execute(os.Stdout, t)
+
+	if err != nil {
+		log.Fatal("failed to write")
+	}
+
 }
